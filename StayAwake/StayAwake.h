56706277b0bf49599e37b61eb1178d4ad0ffd32d
Shridhar Kumar <45252729+shriprem@users.noreply.github.com>
2026-09-21 15:56:13 -0400

    StayAwake pre-release v1.3.0.0 (tag updated for OSSIGN Dispatch)

--- StayAwake/StayAwake.h
+++ StayAwake/StayAwake.h
@@ -6,7 +6,7 @@
 
 #include "resource.h"
 
-constexpr auto LAST_CODE_UPDATE_TIME = "Sep 21 2026 - 10:19:42";
+constexpr auto LAST_CODE_UPDATE_TIME = "Sep 21 2026 - 15:45:46";
 
 
 class CStayAwakeApp : public CWinApp
